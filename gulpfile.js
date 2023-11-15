88dd4319181c0ac23d3bb2ae52a89e1ffcc156ca
AlexeyGre4ko <alexeygre4ko@gmail.com>
2023-11-15 17:58:36 +0300

    refactor: change app1

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,59 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
+const {src, dest, watch, parallel, series} = require('gulp');
+
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const gulppug = require('gulp-pug');
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean');
+const { version } = require('gulp-cli/lib/shared/cli-options');
+const avif = require('gulp-avif');
+const webp = require('gulp-webp');
+const imagemin = require('gulp-imagemin');
+const cached = require('gulp-cached');
+
+function styles() {
+    return src('app/styles/style.scss')
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version']}))
+    .pipe(concat('style.min.css'))
+    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
 }
 
-const sass = require('gulp-sass')(require('sass'));
+function pug() {
+    return src('app/*.pug')
+    .pipe(gulppug())
+    .pipe(dest("app/html"))
+    .pipe(browserSync.stream())
+}
 
-import dartSass from 'sass';
-import gulpSass from 'gulp-sass';
-const sass = gulpSass(dartSass);
+function watching() {
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+    watch(['app/styles/style.scss'], styles)
+    watch(['app/*.pug'], pug)
+}
 
+function cleanBuild() {
+    return src('build')
+    .pipe(clean())
+}
 
-const { src, dest } = require('gulp');
-const pug = require('gulp-pug');
+function building() {
+    return src([
+        'app/css/style.min.css',
+        'app/**/*.html'
+    ], {base : 'app'})
+    .pipe(dest('build'))
+}
 
-exports.views = () => {
-  return src('./src/*.pug')
-    .pipe(
-      pug({
-        // Your options in here.
-      })
-    )
-    .pipe(dest('./dist'));
-};
+exports.styles = styles;
+exports.pug = pug;
+exports.watching = watching;
+exports.build = series(cleanBuild, building)
 
-
-const gulp = require('gulp');
-const svgSprite = require('gulp-svg-sprite');
-
-gulp.src('path/to/assets/*.svg')
-  .pipe(svgSprite(/* ... Insert your configuration here ... */))
-  .pipe(gulp.dest('out'));
-
-
-var concat = require('gulp-concat');
-
-gulp.task('scripts', function() {
-    return gulp.src('./lib/*.js')
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('./dist/'));
-});
-
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
-
-gulp.task('javascript', function() {
-  return gulp.src('src/**/*.js')
-    .pipe(sourcemaps.init())
-      .pipe(concat('all.js'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist'));
-});
-
-exports.default = defaultTask
+exports.default = parallel(styles, pug, watching);
