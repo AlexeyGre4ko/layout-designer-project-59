92df764debaf0a09c3cbdf798bd7ed836abb27bf
AlexeyGre4ko <alexeygre4ko@gmail.com>
2023-11-16 19:52:49 +0300

    refactor: change app2

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,49 @@ const { version } = require('gulp-cli/lib/shared/cli-options');
 const avif = require('gulp-avif');
 const webp = require('gulp-webp');
 const imagemin = require('gulp-imagemin');
-const cached = require('gulp-cached');
+const newer = require('gulp-newer');
+const svgSprite = require('gulp-svg-sprite');
+const fonter = require('gulp-fonter');
+const ttf2woff2 = require('gulp-ttf2woff2');
+
+function fonts {
+    return src('app/fonts/src/*.*')
+    .pipe(fonter({
+        formats: ['woff', 'ttf']
+    }))
+    .pipe(src('app/fonts/*.ttf'))
+    .pipe(ttf2woff2())
+    .pipe(dest('app/fonts'))
+}
+
+function images() {
+    return src(['app/images/src/*.*', '!app/images/src/*.svg'])
+    .pipe(newer('app/images/build'))
+    .pipe(avif({ quality: 50 }))
+
+    .pipe(src('app/images/src/*.*'))
+    .pipe(newer('app/images/build'))
+    .pipe(webp())
+
+    .pipe(src('app/images/src/*.*'))
+    .pipe(newer('app/images/build'))
+    .pipe(imagemin())
+
+    .pipe(dest('app/images/build'))
+}
+
+function sprite() {
+    return src('app/images/build/*.svg')
+    .pipe(svgSprite({
+        mode: {
+            stack: {
+                sprite: '../sprite.svg',
+                example: true
+            }
+        }
+    }))
+    .pipe(dest('app/images/build'))
+}
 
 function styles() {
     return src('app/styles/style.scss')
@@ -35,6 +77,7 @@ function watching() {
         }
     });
     watch(['app/styles/style.scss'], styles)
+    watch(['app/images/src'], images)
     watch(['app/*.pug'], pug)
 }
 
@@ -46,14 +89,18 @@ function cleanBuild() {
 function building() {
     return src([
         'app/css/style.min.css',
+        'app/images/build/*.*',
         'app/**/*.html'
     ], {base : 'app'})
     .pipe(dest('build'))
 }
 
 exports.styles = styles;
+exports.fonts = fonts;
+exports.images = images;
+exports.sprite = sprite;
 exports.pug = pug;
 exports.watching = watching;
 exports.build = series(cleanBuild, building)
 
-exports.default = parallel(styles, pug, watching);
+exports.default = parallel(styles, images, pug, watching);
